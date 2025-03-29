d2545609a9b1a390174515463e7b6c22452117b5
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-03-29 18:44:24 -0300

    Creación RegisterController, Service y DTO necesario

---
+++ src/main/kotlin/controllers/RegisterController.kt
@@ -0,0 +1,18 @@
+package controllers
+
+import dto.register.RegisterRequestDTO
+import org.springframework.web.bind.annotation.*
+import services.RegisterService
+
+@RestController
+@RequestMapping("/registration")
+@CrossOrigin("*")
+
+class RegisterController(
+    val registerService: RegisterService){
+
+    @PostMapping()
+    fun registerUser(@RequestBody registerData: RegisterRequestDTO)/*: RegisterResponseDTO */ =
+        registerService.registerUser(registerData)
+
+}
---
+++ src/main/kotlin/dto/register/RegisterDTO.kt
@@ -0,0 +1,9 @@
+package dto.register
+
+data class RegisterRequestDTO(
+    var email: String,
+    var password: String)
+
+/* fun RegisterRequestDTO.fromDTO() : User {} */
+
+/* data class RegisterResponseDTO() */
