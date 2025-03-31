b8ec08d8d20b2db4cd0f35c955be7e9aa874b691
kirinico <kirinico@gmail.com>
2025-03-31 10:34:06 +0900

    se implemento las validaciones en User

--- src/main/kotlin/models/user.kt
+++ src/main/kotlin/models/user.kt
@@ -1,5 +1,6 @@
 package models
 
+import exceptions.BusinessException
 import java.time.LocalDate
 
 abstract class User : Id {
@@ -14,6 +15,24 @@ abstract class User : Id {
     //var address : Address
 
     override fun validate() {
-
+        this.validateCommonFields()
     }
+
+    private fun validateCommonFields() {
+        if (mail.isBlank() || !mail.contains("@"))
+            throw BusinessException("El mail no es válido")
+        if (name.isBlank())
+            throw BusinessException("El nombre no puede estar vacío")
+        if (lastName.isBlank())
+            throw BusinessException("El apellido no puede estar vacío")
+        if (password.length < 6)
+            throw BusinessException("La contraseña debe tener al menos 6 caracteres")
+        if (dni.toString().length != 8)
+            throw BusinessException("El DNI debe tener 8 dígitos")
+        if (!dateBirth.isBefore(LocalDate.now()))
+            throw BusinessException("La fecha de nacimiento debe ser válida")
+        if (!dateBirth.plusYears(18).isBefore(LocalDate.now()))
+            throw BusinessException("El usuario debe ser mayor a 18 años")
+    }
+
 }
