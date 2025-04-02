cbb749469dd7d2ddcb0109ee5949d5f1f7f892b9
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-02 15:34:23 -0300

    Agrego abstracción. Por ahora, sólo devuelve un data class con mail y password validados.

--- src/main/kotlin/services/RegisterService.kt
+++ src/main/kotlin/services/RegisterService.kt
@@ -1,6 +1,6 @@
 package services
 
-import dto.register.RegisterRequestDTO
+import dto.register.*
 import exceptions.BusinessException
 import org.springframework.stereotype.Service
 import utils.mailSender.MailObserver
@@ -8,14 +8,15 @@ import utils.mailSender.MailObserver
 @Service
 class RegisterService (private val mailObserver: MailObserver) {
 
-    fun registerCustomer(registerData: RegisterRequestDTO) = registrationProcess(registerData)
+    fun registerCustomer(registerData: RegisterRequestDTO): RegisterRequestDTO = registrationProcess(registerData)
 
-    fun registerProfessional(registerData: RegisterRequestDTO) = registrationProcess(registerData)
+    fun registerProfessional(registerData: RegisterRequestDTO): RegisterRequestDTO = registrationProcess(registerData)
 
     fun validRegisterData(registerData: RegisterRequestDTO): Boolean = registerData.email.isNotEmpty() && registerData.password.isNotEmpty()
     
-    fun registrationProcess(registerData: RegisterRequestDTO){
+    fun registrationProcess(registerData: RegisterRequestDTO): RegisterRequestDTO {
         if (!this.validRegisterData(registerData)) throw BusinessException("Invalid data")
         mailObserver.sendRegistrationMailTo(registerData.email.trim())
+        return registerData
     }
 }
