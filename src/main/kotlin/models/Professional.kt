bb94bb61f479988ffc1b97765815755f138110f9
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-02 18:18:34 -0300

    Agrego validaciones necesarias para crear al Professional

--- src/main/kotlin/models/Professional.kt
+++ src/main/kotlin/models/Professional.kt
@@ -1,12 +1,8 @@
 package models
 
+import exceptions.BusinessException
 import java.time.LocalDate
-
-enum class Profession {
-    JARDINERO,
-    GASISTA,
-    ELECTRICISTA
-}
+import java.time.Period
 
 class Professional(
     override var id: Int,
@@ -17,12 +13,29 @@ class Professional(
     override var dni: Int,
     override var avatar: String,
     override var dateBirth: LocalDate,
-    var professions: MutableSet<Profession>,
+    var professions: Set<Profession>,
     var certificates: MutableMap<Profession, List<String>>,
-    var balace: Double,
+    var balance: Double,
     var debt: Double,
 ) : User(){
 
     private fun addConfirmedJob(): Boolean = TODO("Implement me")
     private fun removeConfirmedJob(): Boolean = TODO("Implement me")
+
+    override fun validate(): Unit{
+        if (mail.trim().isBlank()) throw BusinessException("Email cannot be empty")
+        if (name.trim().isBlank()) throw BusinessException("Name cannot be empty")
+        if (lastName.trim().isBlank()) throw BusinessException("Last name cannot be empty")
+        if (password.trim().isBlank()) throw BusinessException("Password cannot be empty")
+        if (!this.validDNI()) throw BusinessException("Invalid DNI")
+        if (avatar.trim().isBlank()) throw BusinessException("Avatar cannot be empty")
+        if(!this.isAdult(dateBirth)) throw BusinessException("Must be 18 or older")
+    }
+
+    private fun validDNI(): Boolean {
+        val dniToString = dni.toString()
+        return dniToString.length == 8 && dniToString.all { it.isDigit() }
+    }
+
+    private fun isAdult(dateBirth: LocalDate): Boolean = Period.between(dateBirth, LocalDate.now()).years >= 18
 }
