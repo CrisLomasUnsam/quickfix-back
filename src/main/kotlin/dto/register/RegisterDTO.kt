998d33ecc3195ce1b8d0af7bbcc57421d36fcea2
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-02 15:35:44 -0300

    Remuevo función de ext. innecesaria

--- src/main/kotlin/dto/register/RegisterDTO.kt
+++ src/main/kotlin/dto/register/RegisterDTO.kt
@@ -1,13 +1,6 @@
 package dto.register
 
-import models.User
-
 data class RegisterRequestDTO(
     var email: String,
-    var password: String)
-
-fun RegisterRequestDTO.fromDTO() : User {
-    return User()
-}
-
-/* data class RegisterResponseDTO() */
+    var password: String
+)
