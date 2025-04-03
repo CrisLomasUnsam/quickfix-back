2c9542e2916d382645beb74961cfa2946478ff1d
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-04-03 10:46:02 -0300

    Correccion en tests MailSender

--- src/main/kotlin/quickfix/utils/mailSender/MailObserver.kt
+++ src/main/kotlin/quickfix/utils/mailSender/MailObserver.kt
@@ -2,6 +2,6 @@ package quickfix.utils.mailSender
 import org.springframework.stereotype.Component
 
 @Component
-class MailObserver(val mailSender: SmtpMailSender){
+class MailObserver(val mailSender: IMailSender){
     fun sendRegistrationMailTo(address: String) = mailSender.sendEmail(Mail(to = address))
 }
--- src/test/kotlin/mocks/MailSenderSpec.kt
+++ src/test/kotlin/mocks/MailSenderSpec.kt
@@ -7,18 +7,18 @@ import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import io.mockk.mockk
 import io.mockk.verify
-import quickfix.utils.mailSender.Mail
-import quickfix.utils.mailSender.MailObserver
-import quickfix.utils.mailSender.MailSender
+import quickfix.models.Address
+import quickfix.models.Gender
 import quickfix.services.RegisterService
-import utils.mailSender.*
+import quickfix.utils.mailSender.*
+import java.time.LocalDate
 
 
 class MailSenderSpec: DescribeSpec({
     isolationMode = IsolationMode.InstancePerTest
 
     describe("given an email address") {
-        val mockedMailSender = mockk<MailSender>(relaxUnitFun = true)
+        val mockedMailSender = mockk<IMailSender>(relaxUnitFun = true)
         val mailObserver = MailObserver(mailSender = mockedMailSender)
         val registerService = RegisterService(mailObserver)
 
@@ -58,12 +58,27 @@ class MailSenderSpec: DescribeSpec({
 
         it("doesn't send the registration email to an invalid address") {
             val invalidAddress = ""
-            val registerData = RegisterRequestDTO(password = "123", email = invalidAddress)
+            val registerData = RegisterRequestDTO(
+                password = "123",
+                mail = invalidAddress,
+                name = "Carlos",
+                lastName = "Perez",
+                dni = 11222333,
+                avatar = "http://...",
+                dateBirth = LocalDate.of(2001,1, 1),
+                gender = Gender.MALE,
+                age = 18,
+                address = Address(
+                    street = "Avenida siempre viva 123",
+                    city = "Springfield",
+                    zipCode = "01234"
+                )
+            )
             try {
                 registerService.registerCustomer(registerData)
                 throw AssertionError("Exception should have been thrown for invalid data.")
             } catch (e: Exception) {
-                e.message shouldBe "Invalid data"
+                e.message shouldBe "El email no es válido."
             }
             verify(exactly = 0) { mockedMailSender.sendEmail(any()) }
         }
