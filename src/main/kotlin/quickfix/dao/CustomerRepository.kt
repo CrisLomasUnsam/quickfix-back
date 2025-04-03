a281d243bb1f87ba0cce4f535ae47e8840a43872
NannVargas <nvargas@estudiantes.unsam.edu.ar>
2025-04-03 15:47:41 -0300

    REFACTOR DE  CustomerRepository

--- src/main/kotlin/quickfix/dao/CustomerRepository.kt
+++ src/main/kotlin/quickfix/dao/CustomerRepository.kt
@@ -1,9 +1,9 @@
 package quickfix.dao
 
 import org.springframework.stereotype.Component
-import quickfix.models.Professional
+import quickfix.models.Customer
 
 @Component
-class CustomerRepository: Repository<Professional>() {
+class CustomerRepository: Repository<Customer>() {
 
 }
