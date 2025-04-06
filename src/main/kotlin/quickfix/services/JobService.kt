d53e69b7302da2c3456e2dc347fd3a93b404891b
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-06 19:15:26 -0300

    Agrego placeholders para metodos necesarios luego

--- src/main/kotlin/quickfix/services/JobService.kt
+++ src/main/kotlin/quickfix/services/JobService.kt
@@ -1,11 +1,21 @@
 package quickfix.services
 
 import org.springframework.stereotype.Service
+import quickfix.dao.JobRepository
+import quickfix.dto.job.JobUpdateDTO
 import quickfix.models.Job
 
 @Service
-class JobService {
-    fun getJobById(id: Int): Job {
+class JobService(
+    val jobRepository: JobRepository
+){
+    fun createJob(job: Job) = jobRepository.create(job)
 
-    }
+    fun deleteJob(job: Job) = jobRepository.delete(job)
+
+    fun getJobById(id: Int): Job? = jobRepository.getById(id)
+
+    fun setJobAsDone(id: Int) = jobRepository.setToDone(id)
+
+    fun setJobAsCancelled(id: Int) = jobRepository.setToCancelled(id)
 }
