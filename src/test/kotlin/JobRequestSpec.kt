dfd5b46c63b6f1b0fbfc1a375e04d2cd7832b1d7
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-06 17:36:10 -0300

    Creo Spec

---
+++ src/test/kotlin/JobRequestSpec.kt
@@ -0,0 +1,49 @@
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import mocks.createCustomerMock
+import quickfix.dto.job.JobRequestDTO
+import quickfix.dto.job.toJobRequest
+import quickfix.models.Profession
+import quickfix.utils.exceptions.BusinessException
+
+
+class JobRequestSpec: DescribeSpec({
+    isolationMode = IsolationMode.InstancePerTest
+
+    describe("JobRequestDTO"){
+        val customer = createCustomerMock().customer
+        val profession = Profession.GASISTA
+        val detail = "Detalle"
+
+        it("should convert to JobRequest") {
+
+            val dto = JobRequestDTO(
+                customer = customer,
+                profession = profession,
+                detail = detail
+            )
+
+            val job = dto.toJobRequest()
+
+            job.customer shouldBe customer
+            job.profession shouldBe profession
+            job.detail shouldBe detail
+        }
+
+        it("should throw exception when detail is blank") {
+            val dto = JobRequestDTO(
+                customer = customer,
+                profession = profession,
+                detail = ""
+            )
+
+            val exception = shouldThrow<BusinessException> {
+                dto.toJobRequest()
+            }
+
+            exception.message shouldBe "Detail is empty"
+        }
+    }
+})
