83350995a4d394de4c1840efe2f18beb1f5f276c
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-04-06 18:48:10 -0300

    Agregado de objeto de dominio Job

---
+++ src/main/kotlin/quickfix/models/Job.kt
@@ -0,0 +1,19 @@
+package quickfix.models
+
+import java.time.LocalDate
+
+class Job : Id {
+
+    override var id: Int = -1
+    lateinit var professional: Professional
+    lateinit var customer: Customer
+    lateinit var date: LocalDate
+    var done: Boolean = false
+    var canceled: Boolean = false
+    var price: Double = 0.0
+    lateinit var distance : Number
+
+    override fun validate() {
+        TODO("Not yet implemented")
+    }
+}
