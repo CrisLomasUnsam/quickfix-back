8f0c485bd39bb3112d504a8760e3d303f69e9fea
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-07 00:28:34 -0300

    Placeholder para un metodo

--- src/main/kotlin/quickfix/services/RatingService.kt
+++ src/main/kotlin/quickfix/services/RatingService.kt
@@ -1,10 +1,11 @@
 package quickfix.services
 
 import org.springframework.stereotype.Service
+import quickfix.models.Rating
 
 @Service
 class RatingService {
-    fun rateProfessional(rating: Any): Any {
+    fun rateProfessional(rating: Rating): Any {
         TODO("Not yet implemented")
     }
 }
