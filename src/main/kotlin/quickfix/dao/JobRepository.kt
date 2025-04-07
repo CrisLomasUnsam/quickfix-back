c2d6aab83fedcadd4cc0b1ec62a9d95d36de9dc4
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-07 01:41:54 -0300

    Interface y metodo polimorficos para buscar jobs de un customer por filtro.

--- src/main/kotlin/quickfix/dao/JobRepository.kt
+++ src/main/kotlin/quickfix/dao/JobRepository.kt
@@ -2,9 +2,12 @@ package quickfix.dao
 
 import org.springframework.stereotype.Component
 import quickfix.models.Job
+import quickfix.utils.SearchParameters
 
 @Component
-class JobRepository: Repository<Job>() {
+class JobRepository(customerRepository: CustomerRepository) : Repository<Job>(
+
+){
 
     fun setToDone(id: Long){
         TODO("Not yet implemented")
@@ -12,5 +15,10 @@ class JobRepository: Repository<Job>() {
 
     fun setToCancelled(id: Long){
         TODO("Not yet implemented")
+    }
+
+    override fun searchByParameters(id: Long, parameters: SearchParameters<Job>): List<Job> {
+        val jobsFilteredByCustomer = this.elements.filter { it.customer.id == id }
+        return jobsFilteredByCustomer.filter { parameters.matches(it) }
     }
 }
