745f0593baecea0c45f4a02ad9c67063b23194f6
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-08 09:48:45 -0300

    Comienzos del put para modifiar UserInfo

--- src/main/kotlin/quickfix/controllers/UserController.kt
+++ src/main/kotlin/quickfix/controllers/UserController.kt
@@ -1,16 +1,21 @@
 package quickfix.controllers
 
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 import quickfix.dto.user.UserDTO
+import quickfix.dto.user.UserModifiedInfoDTO
 import quickfix.services.UserService
 
 @RestController
 @RequestMapping("/user")
 @CrossOrigin(origins = ["*"])
+@Tag(name = "Operaciones relacionadas a la información de los usuarios")
 
 class UserController (val userService: UserService) {
 
     @GetMapping("/data/{id}")
     fun userInfo(@PathVariable id: Long) : UserDTO = UserDTO.toDTO(userService.getUserInfoById(id)!!)
-    
+
+    @PutMapping("/data/edit/{id}")
+    fun updateUserInfo(@PathVariable id: Long, @RequestBody modifiedInfo: UserModifiedInfoDTO) = userService.changeUserInfo(id,modifiedInfo)
 }
