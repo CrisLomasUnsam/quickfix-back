496d88136f85eb5322b149ab168abeb6389c7a07
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-08 16:42:05 -0300

    DTO que acepta campos modificados o null (vacíos)

--- src/main/kotlin/quickfix/dto/user/UserModifiedInfoDTO.kt
+++ src/main/kotlin/quickfix/dto/user/UserModifiedInfoDTO.kt
@@ -2,14 +2,15 @@ package quickfix.dto.user
 
 import quickfix.models.Address
 import quickfix.models.Gender
+import quickfix.models.UserInfo
 import java.time.LocalDate
 
 data class UserModifiedInfoDTO(
-    var mail: String,
-    var name: String,
-    var lastName: String,
-    var dni: Int,
-    var dateBirth: LocalDate,
-    var gender: Gender,
-    var address: Address
+    var mail: String?,
+    var name: String?,
+    var lastName: String?,
+    var dni: Int?,
+    var dateBirth: LocalDate?,
+    var gender: Gender?,
+    var address: Address?
 )
