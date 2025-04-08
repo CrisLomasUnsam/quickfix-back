0b3d1f0d4715970064cb0f38ede37d57bae29af4
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-08 16:40:02 -0300

    Refactor a UserInfo

--- src/test/kotlin/mocks/CustomerMock.kt
+++ src/test/kotlin/mocks/CustomerMock.kt
@@ -1,29 +1,27 @@
 package mocks
 
-import quickfix.models.Address
-import quickfix.models.Customer
-import quickfix.models.Gender
+import quickfix.models.*
 import java.time.LocalDate
 
 data class CustomerMock(
-    val customer: Customer
+    val customer: UserInfo
 )
 
 fun createCustomerMock(): CustomerMock {
-    val mockCustomer = Customer(
-        mail = "test.user@example.com",
-        name = "Test",
-        lastName = "User",
-        password = "securePassword123",
-        dni = 12345678,
-        avatar = "https://example.com/avatar.jpg",
-        dateBirth = LocalDate.of(1995, 5, 20),
-        gender = Gender.MALE, // o Gender.FEMALE
+    val mockCustomer = UserInfo().apply {
+        mail = "test.user@example.com"
+        name = "Test"
+        lastName = "User"
+        password = "securePassword123"
+        dni = 12345678
+        avatar = "https://example.com/avatar.jpg"
+        dateBirth = LocalDate.of(1995, 5, 20)
+        gender = Gender.MALE
         address = Address(
             street = "123 Main Street",
             city = "Testville",
             zipCode = "1234"
         )
-    )
+    }
     return CustomerMock(mockCustomer)
 }
