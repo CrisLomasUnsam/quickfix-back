e45c7266599c423f9d184868f7f1ef5ce66349a5
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-08 16:23:42 -0300

    Metodo que actualiza solo los campos que contengan data. Si llegan campos vacios en el dto, los saltea, NO se persisten nulls

--- src/main/kotlin/quickfix/models/UserInfo.kt
+++ src/main/kotlin/quickfix/models/UserInfo.kt
@@ -66,6 +66,35 @@ class UserInfo : Id {
         this.password == password
 
     fun updateUserInfo(modifiedInfoDTO: UserModifiedInfoDTO) {
-
+        modifiedInfoDTO.mail?.let {
+            val oldMail = this.mail
+            this.mail = it
+            if (!validMail()) {
+                this.mail = oldMail
+                throw BusinessException("Email inválido")
+            }
+        }
+        modifiedInfoDTO.name?.let {
+            if (!validName(it)) throw BusinessException("Nombre sólo puede contener letras")
+            this.name = it
+        }
+        modifiedInfoDTO.lastName?.let {
+            if (!validName(it)) throw BusinessException("Apellido sólo puede contener letras")
+            this.lastName = it
+        }
+        modifiedInfoDTO.dateBirth?.let {
+            val oldDate = this.dateBirth
+            this.dateBirth = it
+            if (!isAdult()) {
+                this.dateBirth = oldDate
+                throw BusinessException("Debe ser mayor de edad")
+            }
+        }
+        modifiedInfoDTO.gender?.let {
+            this.gender = it
+        }
+        modifiedInfoDTO.address?.let {
+            this.address = it
+        }
     }
 }
