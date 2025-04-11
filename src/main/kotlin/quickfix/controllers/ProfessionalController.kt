7c810addb0503a33978d7c312a85ae87c7c2a2e4
NannVargas <nvargas@estudiantes.unsam.edu.ar>
2025-04-10 22:58:09 -0300

    refactor ProfessionalController

--- src/main/kotlin/quickfix/controllers/ProfessionalController.kt
+++ src/main/kotlin/quickfix/controllers/ProfessionalController.kt
@@ -1,25 +1,11 @@
 package quickfix.controllers
 
-import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
-import quickfix.model.Professional
-//import quickfix.dto.professional.ProfessionalDTO
 import quickfix.services.ProfessionalService
 
 @RestController
 @RequestMapping("/professional")
-@CrossOrigin = ["*"]
+@CrossOrigin (origins = ["*"])
 
-class ProfessionalController () {
-
-    val professionalService : ProfessionalService
-
-    @PostMapping("/")
-    fun createProfessional(@RequestBody professional: Professional) =
-        professionalService.create(professional)
-
-    @GetMapping("/{id}")
-    fun getProfessionalById(@PathVariable id: Long) =
-        professionalService.getProfessionalById(id)
-
+class ProfessionalController (val professionalService : ProfessionalService) {
 }
