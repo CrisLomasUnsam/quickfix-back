b8f13f77e6d9677ab500e46b436b19955bbd7c91
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-14 10:20:45 -0300

    Agrego yearAndMonth

--- src/main/kotlin/quickfix/dto/rating/RatingDTO.kt
+++ src/main/kotlin/quickfix/dto/rating/RatingDTO.kt
@@ -1,12 +1,14 @@
 package quickfix.dto.rating
 
 import quickfix.models.Rating
+import java.time.LocalDate
 
 data class RatingDTO(
     val userFromId: Long,
     val userToId: Long,
     val jobId: Long,
     val score: Int,
+    val yearAndMonth: LocalDate,
     val comment: String
 )
 
