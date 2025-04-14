c8f6e3a82c640af486e1199125e7f19674e6fc3d
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-14 17:24:10 -0300

    Agrego EntitiesGraph con su correspondiente optional

--- src/main/kotlin/quickfix/dao/RatingRepository.kt
+++ src/main/kotlin/quickfix/dao/RatingRepository.kt
@@ -1,11 +1,17 @@
 package quickfix.dao
 
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Component
 import quickfix.models.Rating
+import java.util.*
+
 @Component
 interface RatingRepository : CrudRepository<Rating, Long> {
 
-    fun findByUserToId(userId: Long): List<Rating>
-    fun findByUserFromId(userId: Long): List<Rating>
+    @EntityGraph(attributePaths = ["user","job"])
+    fun findByUserToId(userId: Long): Optional<List<Rating>>
+
+    @EntityGraph(attributePaths = ["user","job"])
+    fun findByUserFromId(userId: Long): Optional<List<Rating>>
 }
