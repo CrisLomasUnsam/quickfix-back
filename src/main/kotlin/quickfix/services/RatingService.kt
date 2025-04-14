7f691a4e18ffca3690c65761a95e09d1dccc016f
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-14 17:24:58 -0300

    Agrego tag transactional y termino el rateUser

--- src/main/kotlin/quickfix/services/RatingService.kt
+++ src/main/kotlin/quickfix/services/RatingService.kt
@@ -1,11 +1,13 @@
 package quickfix.services
 
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import quickfix.dao.JobRepository
 import quickfix.dao.RatingRepository
 import quickfix.dao.UserRepository
 import quickfix.dto.rating.RatingDTO
 import quickfix.models.Rating
+import quickfix.utils.exceptions.BusinessException
 
 @Service
 class RatingService(
@@ -13,26 +15,34 @@ class RatingService(
     val jobRepository: JobRepository,
     val ratingRepository: RatingRepository
 ) {
+    @Transactional
     fun rateUser(ratingDTO: RatingDTO) {
-        val userFrom = userRepository.findById(ratingDTO.userFromId)
-        val userTo = userRepository.findById(ratingDTO.userToId)
-        val job = jobRepository.findById(ratingDTO.jobId)
+        val userFrom = userRepository.findById(ratingDTO.userFromId).orElseThrow {
+            BusinessException("Usuario no existe")
+        }
+        val userTo = userRepository.findById(ratingDTO.userToId).orElseThrow {
+            BusinessException("Usuario no existe")
+        }
+        val job = jobRepository.findById(ratingDTO.jobId).orElseThrow {
+            BusinessException("Job no existe")
+        }
         val rating = Rating().apply {
-            this.userFrom
-            this.userTo
-            this.job
-            this.score
-            this.yearAndMonth
-            this.comment
-        }.validate()
+            this.userFrom = userFrom
+            this.userTo = userTo
+            this.job = job
+            this.score = ratingDTO.score
+            this.yearAndMonth = ratingDTO.yearAndMonth
+            this.comment = ratingDTO.comment
+        }.also { it.validate() }
+        ratingRepository.save(rating)
     }
 
     fun findRatingsReceivedByUser(userId: Long): List<Rating> {
-        return ratingRepository.findByUserToId(userId)
+        return ratingRepository.findByUserToId(userId).orElseThrow{ BusinessException("Usuario no existe") }
     }
 
     fun findRatingsMadeByUser(userId: Long): List<Rating> {
-        return ratingRepository.findByUserFromId(userId)
+        return ratingRepository.findByUserFromId(userId).orElseThrow { BusinessException("Usuario no existe") }
     }
 
 }
