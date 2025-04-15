6d5fe88b3ac0260f34246dbc73610008cc8c290d
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-15 08:12:08 -0300

    Agrego el fromDTO con su data class correspondiente para parsear el string recibido a localDate

--- src/main/kotlin/quickfix/dto/rating/EditRatingDTO.kt
+++ src/main/kotlin/quickfix/dto/rating/EditRatingDTO.kt
@@ -1,9 +1,24 @@
 package quickfix.dto.rating
 
+import quickfix.utils.datifyString
 import java.time.LocalDate
 
-data class EditRatingDTO(
+data class EditRating(
     val score: Int?,
     val yearAndMonth: LocalDate?,
     val comment: String?
 )
+
+data class EditRatingDTO(
+    val score: Int?,
+    val yearAndMonth: String?,
+    val comment: String?
+)
+
+fun EditRatingDTO.fromDTO(): EditRating {
+    return EditRating(
+        score = score,
+        yearAndMonth = this.yearAndMonth?.let { datifyString(it) },
+        comment = comment
+    )
+}
