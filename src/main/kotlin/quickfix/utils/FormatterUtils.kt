8e4b5b06cc4c83c5b654ed7a378a9f85a53994d9
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-15 08:10:10 -0300

    Creo el utils para formatear las fechas para enviarla/recibirla del front. Tiene dos metodos, uno para pasar un str a localDate y otro que hace lo inverso. Estan implementados en RatingDTO y EditRatingDTO.

---
+++ src/main/kotlin/quickfix/utils/FormatterUtils.kt
@@ -0,0 +1,10 @@
+package quickfix.utils
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+val ISOformatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM")
+
+fun stringifyDate(fecha: LocalDate): String = fecha.format(formatter)
+fun datifyString(fechaStr: String): LocalDate = LocalDate.parse(fechaStr, ISOformatter)
