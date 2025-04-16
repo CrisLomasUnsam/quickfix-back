a62c4382940a81edaddad277cb5e776f4ff79769
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-16 01:34:31 -0300

    Agrego validaciones para ambos usuarios de la rating esten en el Job y que un usuario no se este valorando a si mismo

--- src/main/kotlin/quickfix/models/Rating.kt
+++ src/main/kotlin/quickfix/models/Rating.kt
@@ -27,11 +27,15 @@ class Rating : Identifier {
         if (!validScore(score)) throw BusinessException("El puntaje no puede ser mayor a 5 o menor a 1")
         if (!validaDate(yearAndMonth)) throw BusinessException("Fecha fuera de rango válido")
         if (!validComment(comment)) throw BusinessException("Debe agregar un comentario")
+        if (!jobIncludesUsers()) throw BusinessException("El job a valorar no contiene a uno/ambos usuarios")
+        if (userFrom.id == userTo.id) throw BusinessException("Un usuario no puede calificarse a sí mismo")
     }
+
+    private fun jobIncludesUsers(): Boolean = setOf(job.customer.id, job.professional.id).containsAll(setOf(userFrom.id, userTo.id))
 
     private fun validComment(comment: String): Boolean = comment.isNotBlank()
 
-    private fun validaDate(date: YearMonth): Boolean = date.isBefore(YearMonth.now()) || date == YearMonth.now()
+    private fun validaDate(date: YearMonth): Boolean = date.year == job.date.year && date.month >= job.date.month
 
     private fun validScore(score: Int): Boolean = score in 1..5
 }
