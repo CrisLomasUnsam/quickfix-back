846377ceb32599839e39014719b5eba5fe1134c4
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-16 01:31:04 -0300

    Se simplifico el formatter y se agregó validación por si llega mal del front

--- src/main/kotlin/quickfix/utils/FormatterUtils.kt
+++ src/main/kotlin/quickfix/utils/FormatterUtils.kt
@@ -1,10 +1,17 @@
 package quickfix.utils
 
+import quickfix.utils.exceptions.BusinessException
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM")
-val ISOformatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM")
 
 fun stringifyDate(fecha: YearMonth): String = fecha.format(formatter)
-fun datifyString(fechaStr: String): YearMonth = YearMonth.parse(fechaStr, ISOformatter)
+fun datifyString(fechaStr: String): YearMonth {
+    return try {
+        YearMonth.parse(fechaStr, formatter)
+    } catch (e: DateTimeParseException) {
+        throw BusinessException("La fecha no tiene el formato esperado: yyyy-MM")
+    }
+}
