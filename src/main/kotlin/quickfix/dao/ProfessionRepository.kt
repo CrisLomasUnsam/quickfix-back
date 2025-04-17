77f37f761300abe85c396f25e4447b2a18204808
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-17 00:25:42 -0300

    Agrego tag y marco Profession como nullable para atraparlo en su service correspondiente

--- src/main/kotlin/quickfix/dao/ProfessionRepository.kt
+++ src/main/kotlin/quickfix/dao/ProfessionRepository.kt
@@ -1,9 +1,11 @@
 package quickfix.dao
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Component
 import quickfix.models.Profession
 
+@Component
 interface ProfessionRepository: CrudRepository<Profession, Long> {
 
-    fun findByNameContainingIgnoreCase(name: String): Profession
+    fun findByNameContainingIgnoreCase(name: String): Profession?
 }
