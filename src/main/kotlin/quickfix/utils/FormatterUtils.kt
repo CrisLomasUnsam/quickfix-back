93c61d04ff9b82d445a190cff81290b9dc88c2a8
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-17 00:21:29 -0300

    Refactor a LocalDate

--- src/main/kotlin/quickfix/utils/FormatterUtils.kt
+++ src/main/kotlin/quickfix/utils/FormatterUtils.kt
@@ -1,17 +1,18 @@
 package quickfix.utils
 
 import quickfix.utils.exceptions.BusinessException
-import java.time.YearMonth
+import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
 
 val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM")
 
-fun stringifyDate(fecha: YearMonth): String = fecha.format(formatter)
-fun datifyString(fechaStr: String): YearMonth {
+fun stringifyDate(fecha: LocalDate): String = fecha.format(formatter)
+fun datifyString(fechaStr: String): LocalDate {
     return try {
-        YearMonth.parse(fechaStr, formatter)
+        LocalDate.parse(fechaStr, formatter)
     } catch (e: DateTimeParseException) {
-        throw BusinessException("La fecha no tiene el formato esperado: yyyy-MM")
+        throw BusinessException("La fecha no tiene el formato esperado: yyyy-MM-dd")
     }
 }
+
