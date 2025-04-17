40797badc0a3185ebd1f46ca11a14389f88b96b0
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-17 00:20:55 -0300

    Remuevo date como campo editable en una Rating

--- src/main/kotlin/quickfix/dto/rating/EditRatingDTO.kt
+++ src/main/kotlin/quickfix/dto/rating/EditRatingDTO.kt
@@ -1,26 +1,18 @@
 package quickfix.dto.rating
 
-import quickfix.utils.datifyString
-import java.time.YearMonth
-
 data class EditRating(
     val score: Int?,
-    val yearAndMonth: YearMonth?,
     val comment: String?
 )
 
 data class EditRatingDTO(
     val score: Int?,
-    val yearAndMonth: String?,
     val comment: String?
 )
 
 fun EditRatingDTO.fromDTO(): EditRating {
     return EditRating(
         score = score,
-        yearAndMonth = yearAndMonth
-            ?.takeIf { it.isNotBlank() }
-            ?.let { datifyString(it) },
         comment = comment
     )
 }
