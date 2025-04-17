bab77f77c4436b82ae66641bb9e7f53b612649ac
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-17 00:28:42 -0300

    Quito EntityGraph innecesario

--- src/main/kotlin/quickfix/dao/RatingRepository.kt
+++ src/main/kotlin/quickfix/dao/RatingRepository.kt
@@ -1,6 +1,5 @@
 package quickfix.dao
 
-import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Component
 import quickfix.models.Rating
@@ -9,9 +8,7 @@ import java.util.*
 @Component
 interface RatingRepository : CrudRepository<Rating, Long> {
 
-    @EntityGraph(attributePaths = ["userFrom","userTo","job"])
     fun findByUserToId(userId: Long): Optional<List<Rating>>
 
-    @EntityGraph(attributePaths = ["userFrom","userTo","job"])
     fun findByUserFromId(userId: Long): Optional<List<Rating>>
 }
