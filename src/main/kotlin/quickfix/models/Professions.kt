408911041eaf28e93504ec6e211dbec7a9a7dbdb
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-18 12:39:48 -0300

    Enum profesiones completo

---
+++ src/main/kotlin/quickfix/models/Professions.kt
@@ -0,0 +1,14 @@
+package quickfix.models
+
+enum class Professions {
+    GASISTA,
+    CARPINTERO,
+    ALBANIL,
+    PINTOR,
+    JARDINERO,
+    PLOMERO,
+    ARQUITECTO,
+    MECANICO,
+    ELECTRICISTA,
+    OTROS
+}
