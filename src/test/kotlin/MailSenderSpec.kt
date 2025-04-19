dee02b83e684b40e5cf4d64a605b36f41c3a0eee
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-19 17:56:28 -0300

    Fix tests

--- src/test/kotlin/MailSenderSpec.kt
+++ src/test/kotlin/MailSenderSpec.kt
@@ -3,6 +3,7 @@ import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
+import io.mockk.every
 import io.mockk.verify
 import mocks.createMailSenderMock
 import quickfix.models.Address
@@ -17,7 +18,7 @@ class MailSenderSpec: DescribeSpec({
     isolationMode = IsolationMode.InstancePerTest
 
     describe("given an email address") {
-        val (mockedMailSender, mockedRegisterService) = createMailSenderMock()
+        val (mockedMailSender, mockedRegisterService, mockedUserRepository) = createMailSenderMock()
 
         it("sends the registration email if it's valid") {
             val address = "valid@user.com"
@@ -70,6 +71,7 @@ class MailSenderSpec: DescribeSpec({
                     zipCode = "01234"
                 }
             )
+            every { mockedUserRepository.findByMail(any()) } returns null
             try {
                 mockedRegisterService.registerUser(registerData)
                 throw AssertionError("Exception should have been thrown for invalid data.")
