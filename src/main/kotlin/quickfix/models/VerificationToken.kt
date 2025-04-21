a2638dcd1eb35c0a3d21b68a28218c08ae4bdcb4
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-21 01:05:32 -0300

    Entidad + repo token para crear el link y validar el usuario registrándose

---
+++ src/main/kotlin/quickfix/models/VerificationToken.kt
@@ -0,0 +1,21 @@
+package quickfix.models
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+class VerificationToken: Identifier {
+
+    @Id @GeneratedValue
+    override var id: Long = -1
+
+
+    @OneToOne
+    @JoinColumn(nullable = false)
+    lateinit var user: User
+
+    lateinit var token: String
+    var expiryDate: LocalDateTime = LocalDateTime.now().plusMinutes(60)
+
+    override fun validate() {}
+}
