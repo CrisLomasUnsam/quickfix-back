4671b2c496cbf2331408487a085bed8f929c6493
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-21 01:03:49 -0300

    Refactor -> contenido mail real

--- src/main/kotlin/quickfix/utils/mailSender/MailObserver.kt
+++ src/main/kotlin/quickfix/utils/mailSender/MailObserver.kt
@@ -1,7 +1,28 @@
 package quickfix.utils.mailSender
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
+import quickfix.utils.events.OnRegistrationCompletedEvent
 
 @Component
 class MailObserver(val mailSender: IMailSender){
-    fun sendRegistrationMailTo(address: String) = mailSender.sendEmail(Mail(to = address))
+    @EventListener
+    fun handleRegistrationCompleted(event: OnRegistrationCompletedEvent) {
+        val mail = Mail(
+            to = event.user.mail,
+            content = """ 
+                <html>
+                    <body>
+                        <p>¡Hola ${event.user.name}!</p>
+                    
+                        <p>Gracias por registrarte en QuickFix.</p>
+                        <p>Para confirmar tu cuenta, haz click en el siguiente link:</p>  
+                        <p><a href="${event.confirmationURL}">${event.confirmationURL}</a></p>
+           
+                        <p>Si no creaste esta cuenta, ignora este mensaje.</p>
+                    </body>
+                </html>
+            """.trimIndent()
+        )
+        mailSender.sendEmail(mail)
+    }
 }
