870afeecb073203397fa70afd161dfeb6adb061f
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-04-21 18:26:44 -0300

    Creo DTOs necesarios

---
+++ src/main/kotlin/quickfix/dto/professional/FinancesDTO.kt
@@ -0,0 +1,6 @@
+package quickfix.dto.professional
+
+data class FinancesDTO (
+    val balanceActual: Double,
+    val deudaActual: Double
+)
---
+++ src/main/kotlin/quickfix/dto/professional/NewCertificateDTO.kt
@@ -0,0 +1,6 @@
+package quickfix.dto.professional
+
+data class NewCertificateDTO(
+    val profession: String,
+    val img: String
+)
