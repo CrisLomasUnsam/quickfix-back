fb4bcd2e0548ca8854e45bd71d8160eefff602de
kirinico <kirinico@gmail.com>
2025-04-23 14:44:51 +0900

    refactor jobDTO

--- src/main/kotlin/quickfix/dto/job/JobDTO.kt
+++ src/main/kotlin/quickfix/dto/job/JobDTO.kt
@@ -5,20 +5,16 @@ import java.time.LocalDate
 
 data class JobDTO(
     val id: Long,
-    val professionalId: Long,
-    val customerId: Long,
     val date: LocalDate,
-    val professionName: String,
+    val professionalName: String,
     val status: String,
     val price: Double
 )
 
 fun toDto(job: Job): JobDTO = JobDTO(
-    id             = job.id,
-    professionalId = job.professional.id,
-    customerId     = job.customer.id,
-    date           = job.date,
-    professionName = job.profession.name,
-    status         = job.status.name,
-    price          = job.price
+    id               = job.id,
+    date             = job.date,
+    professionalName = job.professional.name,
+    status           = job.status.name,
+    price            = job.price
 )
