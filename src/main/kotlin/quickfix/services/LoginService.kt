652edfdf89e080f387b2cb78a07d8684f2835fb8
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-04-25 13:03:48 -0300

    Refactor visual de código

--- src/main/kotlin/quickfix/services/LoginService.kt
+++ src/main/kotlin/quickfix/services/LoginService.kt
@@ -8,18 +8,18 @@ import quickfix.utils.exceptions.InvalidCredentialsException
 
 @Component
 class LoginService (
-
     val userRepository: UserRepository
 ) {
+
     fun loginUser(loginDTO: LoginDTO) {
         val user = findUserByMail(loginDTO.mail)
-
-        if (!validPassword(user, loginDTO)) {
+        if (!validPassword(user, loginDTO))
             throw InvalidCredentialsException()
-        }
     }
 
-    private fun findUserByMail(mail: String) = userRepository.findByMail(mail)?: throw InvalidCredentialsException()
+    private fun findUserByMail(mail: String) =
+        userRepository.findByMail(mail)?: throw InvalidCredentialsException()
 
-    private fun validPassword(user: User, loginDTO: LoginDTO): Boolean = user.verifyPassword(loginDTO.password)
+    private fun validPassword(user: User, loginDTO: LoginDTO): Boolean =
+        user.verifyPassword(loginDTO.password)
 }
