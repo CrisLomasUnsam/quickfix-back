37358c88bf258145eb48614786cda63748bc2a5f
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-04-25 12:00:55 -0300

    Agregado de decoradores JsonProperties + JsonCreator constructor

--- src/main/kotlin/quickfix/dto/job/jobOffer/CreateJobOfferDTO.kt
+++ src/main/kotlin/quickfix/dto/job/jobOffer/CreateJobOfferDTO.kt
@@ -1,12 +1,30 @@
 package quickfix.dto.job.jobOffer
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 
-data class CreateJobOfferDTO(
+
+data class CreateJobOfferDTO @JsonCreator constructor (
+
+    @JsonProperty
     var customerId : Long,
+
+    @JsonProperty
     var professionId : Long,
+
+    @JsonProperty
     var professionalId: Long,
+
+    @JsonProperty
     var price: Double,
+
+    @JsonProperty
     var distance: Double,
+
+    @JsonProperty
     var estimatedArriveTime: Int,
-    var availability: Int,
+
+    @JsonProperty
+    var availability: Int
+
     )
