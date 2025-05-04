0b0bf34c7f9e6c2e7c3a76e95c117459636ad691
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-05-04 00:58:50 -0300

    Repo/Service/Controller de Rating Paginado

--- src/main/kotlin/quickfix/dao/RatingRepository.kt
+++ src/main/kotlin/quickfix/dao/RatingRepository.kt
@@ -1,16 +1,16 @@
 package quickfix.dao
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Component
 import quickfix.models.Rating
-import java.util.*
 
 @Component
-interface RatingRepository : CrudRepository<Rating, Long> {
+interface RatingRepository : PagingAndSortingRepository<Rating, Long>, CrudRepository<Rating, Long> {
 
-    fun findByUserToId(userId: Long): Optional<List<Rating>>
+    fun findByUserToId(userId: Long, pageable: Pageable): Page<Rating>
 
-    fun findByUserFromId(userId: Long): Optional<List<Rating>>
-
-
+    fun findByUserFromId(userId: Long, pageable: Pageable): Page<Rating>
 }
