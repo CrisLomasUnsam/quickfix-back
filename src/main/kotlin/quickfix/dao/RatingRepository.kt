6c8967fe37bd388a3e109f0541aff501272ea4f5
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-05-04 14:40:54 -0300

    Modificación en herencia de RatingRepository: JpaRepository es la combinación de PagingAndSortedRepository + CrudRepository

--- src/main/kotlin/quickfix/dao/RatingRepository.kt
+++ src/main/kotlin/quickfix/dao/RatingRepository.kt
@@ -2,15 +2,14 @@ package quickfix.dao
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
-import org.springframework.data.repository.CrudRepository
-import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Component
 import quickfix.models.Rating
 
 @Component
-interface RatingRepository : PagingAndSortingRepository<Rating, Long>, CrudRepository<Rating, Long> {
+interface RatingRepository : JpaRepository<Rating, Long> {
 
-    fun findByUserToId(userId: Long, pageable: Pageable): Page<Rating>
+    fun findAllByUserToId(userId: Long, pageable: Pageable): Page<Rating>
 
-    fun findByUserFromId(userId: Long, pageable: Pageable): Page<Rating>
+    fun findAllByUserFromId(userId: Long): List<Rating>
 }
