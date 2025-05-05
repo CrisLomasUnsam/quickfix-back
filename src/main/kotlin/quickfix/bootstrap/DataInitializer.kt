606aa7afa32273ba378efe616f9fa20001911af0
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-05-05 01:25:18 -0300

    Eliminación de dependencias sin usar de DataInitializer

--- src/main/kotlin/quickfix/bootstrap/DataInitializer.kt
+++ src/main/kotlin/quickfix/bootstrap/DataInitializer.kt
@@ -1,7 +1,5 @@
 package quickfix.bootstrap
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
