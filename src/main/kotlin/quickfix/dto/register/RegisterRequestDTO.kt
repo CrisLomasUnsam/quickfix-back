5be6bf85faea76c46ee747ea178bc1f57599da7b
kirinico <kirinico@gmail.com>
2025-05-06 15:13:35 +0900

    se agrego el Base64.getDecoder() para avatar

--- src/main/kotlin/quickfix/dto/register/RegisterRequestDTO.kt
+++ src/main/kotlin/quickfix/dto/register/RegisterRequestDTO.kt
@@ -4,6 +4,7 @@ import quickfix.models.Address
 import quickfix.models.Gender
 import quickfix.models.User
 import quickfix.utils.datifyStringWithDay
+import java.util.Base64
 
 class RegisterRequestDTO (
     var mail: String,
@@ -19,12 +20,14 @@ class RegisterRequestDTO (
 
 fun RegisterRequestDTO.toUser() : User {
     val request : RegisterRequestDTO = this
+
     return User().apply {
+
         mail = request.mail.trim()
         name = request.name.trim()
         lastName = request.lastName.trim()
         dni = request.dni
-        avatar = request.avatar.trim()
+        avatar =  Base64.getDecoder().decode(request.avatar.trim())
         dateBirth = datifyStringWithDay(request.dateBirth)
         gender = request.gender
         address = request.address
