3a65ee891dfb5c5e51df63166f480d04ccdd9a72
kirinico <kirinico@gmail.com>
2025-05-06 15:12:30 +0900

    se agrego Base64.getEncoder para avatar

--- src/main/kotlin/quickfix/dto/professional/ProfessionalDTO.kt
+++ src/main/kotlin/quickfix/dto/professional/ProfessionalDTO.kt
@@ -2,7 +2,7 @@ package quickfix.dto.professional
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
-
+import java.util.Base64
 data class ProfessionalDTO @JsonCreator constructor(
     @JsonProperty("id")             var id: Long,
     @JsonProperty("name")           var name: String,
@@ -13,16 +13,20 @@ data class ProfessionalDTO @JsonCreator constructor(
     @JsonProperty("hasVehicle")     var hasVehicle: Boolean
 ){
     companion object {
-        fun fromUser(user: quickfix.models.User, averageRating: Double): ProfessionalDTO =
-            ProfessionalDTO(
-                id            = user.id,
-                name          = user.name,
-                lastName      = user.lastName,
-                avatar        = user.avatar,
-                verified      = user.verified,
+
+        fun fromUser(user: quickfix.models.User, averageRating: Double): ProfessionalDTO {
+            val avatarBase64 = user.avatar.let { Base64.getEncoder().encodeToString(it) }
+                ?: ""
+           return ProfessionalDTO(
+                id = user.id,
+                name = user.name,
+                lastName = user.lastName,
+                avatar = avatarBase64,
+                verified = user.verified,
                 averageRating = averageRating,
-                hasVehicle    = user.professionalInfo.hasVehicle
+                hasVehicle = user.professionalInfo.hasVehicle
             )
+        }
     }
 }
 
