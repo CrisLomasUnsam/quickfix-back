d004148d14f0a9881ec6a86f3ef20afcf379d0b6
kirinico <kirinico@gmail.com>
2025-05-07 09:37:39 +0900

    se elimina la referencia a avatar en ProfessionalDTO

--- src/main/kotlin/quickfix/dto/professional/ProfessionalDTO.kt
+++ src/main/kotlin/quickfix/dto/professional/ProfessionalDTO.kt
@@ -2,12 +2,11 @@ package quickfix.dto.professional
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
-import java.util.Base64
+
 data class ProfessionalDTO @JsonCreator constructor(
     @JsonProperty("id")             var id: Long,
     @JsonProperty("name")           var name: String,
     @JsonProperty("lastName")       var lastName: String,
-    @JsonProperty("avatar")         var avatar: String,
     @JsonProperty("verified")       var verified: Boolean,
     @JsonProperty("averageRating")  var averageRating: Double,
     @JsonProperty("hasVehicle")     var hasVehicle: Boolean
@@ -15,13 +14,10 @@ data class ProfessionalDTO @JsonCreator constructor(
     companion object {
 
         fun fromUser(user: quickfix.models.User, averageRating: Double): ProfessionalDTO {
-            val avatarBase64 = user.avatar.let { Base64.getEncoder().encodeToString(it) }
-                ?: ""
            return ProfessionalDTO(
                 id = user.id,
                 name = user.name,
                 lastName = user.lastName,
-                avatar = avatarBase64,
                 verified = user.verified,
                 averageRating = averageRating,
                 hasVehicle = user.professionalInfo.hasVehicle
