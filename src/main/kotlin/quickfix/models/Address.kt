65b08157615e33540e09ec844ce593863121d54d
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-05-07 13:50:22 -0300

    agrego nuevos campos, su validación y dtos

--- src/main/kotlin/quickfix/models/Address.kt
+++ src/main/kotlin/quickfix/models/Address.kt
@@ -15,18 +15,26 @@ class Address : Identifier {
     override var id: Long = -1
 
     lateinit var street: String
-    lateinit var city: String
+    var optional: String = ""
     lateinit var zipCode: String
+    lateinit var state: String
+    lateinit var city: String
 
     fun updateAddressInfo(modifiedAddressDTO: AddressDTO) {
         modifiedAddressDTO.street?.takeIf { it.isNotBlank() }?.let {
             this.street = it
         }
-        modifiedAddressDTO.city?.takeIf { it.isNotBlank() }?.let {
-            this.city = it
+        modifiedAddressDTO.optional?.takeIf { it.isNotBlank() }?.let {
+            this.optional = it
         }
         modifiedAddressDTO.zipCode?.takeIf { it.isNotBlank() }?.let {
             this.zipCode = it
+        }
+        modifiedAddressDTO.state?.takeIf { it.isNotBlank() }?.let {
+            this.state = it
+        }
+        modifiedAddressDTO.city?.takeIf { it.isNotBlank() }?.let {
+            this.city = it
         }
 
     }
@@ -34,6 +42,7 @@ class Address : Identifier {
     override fun validate() {
         if (!validName(street)) throw BusinessException("La calle debe contener sólo letras")
         if (!validName(city)) throw BusinessException("La ciudad debe contener sólo letras")
+        if (!validName(state)) throw BusinessException("La provincia debe contener sólo letras")
         if (!validZipCode()) throw BusinessException("El código postal deben contener sólo números")
     }
 
