8480d4ba9f70da1737a5144d9b775adffb0b2407
kirinico <kirinico@gmail.com>
2025-05-14 15:08:43 +0900

    metodo con query q te crea los amount_ratings

--- src/main/kotlin/quickfix/dao/RatingRepository.kt
+++ src/main/kotlin/quickfix/dao/RatingRepository.kt
@@ -3,12 +3,16 @@ package quickfix.dao
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Component
 import quickfix.models.Rating
-import java.util.*
+import quickfix.models.RatingStatsProjection
 
 @Component
 interface RatingRepository : JpaRepository<Rating, Long> {
+
+//
 
     fun findAllByUserToId(userId: Long, pageable: Pageable): Page<Rating>
 
@@ -16,4 +20,19 @@ interface RatingRepository : JpaRepository<Rating, Long> {
 
     fun existsByJobIdAndUserFromId(jobId: Long, userFromId: Long): Boolean
 
+    @Query(
+        value = """
+        SELECT count(*) as total, 
+          COALESCE(AVG(r.score), 0) as avg, 
+          COALESCE(SUM(CASE WHEN r.score = 1 THEN 1 END), 0) AS amount_rating1,
+          COALESCE(SUM(CASE WHEN r.score = 2 THEN 1 END), 0) AS amount_rating2,
+          COALESCE(SUM(CASE WHEN r.score = 3 THEN 1 END), 0) AS amount_rating3,
+          COALESCE(SUM(CASE WHEN r.score = 4 THEN 1 END), 0) AS amount_rating4,
+          COALESCE(SUM(CASE WHEN r.score = 5 THEN 1 END), 0) AS amount_rating5
+        FROM ratings r 
+        WHERE r.user_to_id = :userId
+        """,
+        nativeQuery = true
+    )
+    fun findRatingStatsForUser(@Param("userId") userId: Long):  RatingStatsProjection
 }
