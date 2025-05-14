0a12a6d982aec25e20008e58ba4e7798b5453e2d
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-05-13 23:38:59 -0300

    Modificación en MessageRespondeDTO para que devuelva únicamente el día y la hora del mensaje del chat (mediante utilización de método stringifyDateTimeWithoutYear)

--- src/main/kotlin/quickfix/dto/chat/RedisMessageDTO.kt
+++ src/main/kotlin/quickfix/dto/chat/RedisMessageDTO.kt
@@ -2,9 +2,8 @@ package quickfix.dto.chat
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
-import quickfix.utils.CustomDateTimeFormatter
 import quickfix.utils.dateTimeFromTimestamp
-import quickfix.utils.stringifyDateTime
+import quickfix.utils.stringifyDateTimeWithoutYear
 
 data class RedisMessageDTO @JsonCreator constructor(
 
@@ -22,5 +21,5 @@ data class RedisMessageDTO @JsonCreator constructor(
 fun RedisMessageDTO.toMessageResponseDTO(requesterIsCustomer : Boolean) = MessageResponseDTO(
     message = this.message,
     itsMine = (senderIsCustomer && requesterIsCustomer) || (!senderIsCustomer && !requesterIsCustomer),
-    datetime = stringifyDateTime(dateTimeFromTimestamp(this.timestamp), CustomDateTimeFormatter)
+    datetime = stringifyDateTimeWithoutYear(dateTimeFromTimestamp(this.timestamp))
 )
