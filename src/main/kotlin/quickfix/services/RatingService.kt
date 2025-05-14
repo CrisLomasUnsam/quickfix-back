531415f5a4691ad0983de280f026cc59dac1bcdb
kirinico <kirinico@gmail.com>
2025-05-15 08:43:54 +0900

    refactor

--- src/main/kotlin/quickfix/services/RatingService.kt
+++ src/main/kotlin/quickfix/services/RatingService.kt
@@ -69,8 +69,7 @@ class RatingService(
     fun getSeeProfile(currentUserId: Long): UserProfileDTO {
         val user = userService.getById(currentUserId)
         val totalJobs = jobService.countFinishedJobsForProfessional(user.id)
-        val professions  = user.professionalInfo.activeProfessionNames()
         val stats = ratingRepository.findRatingStatsForUser(user.id)
-        return UserProfileDTO.from(user, totalJobs, professions, stats)
+        return UserProfileDTO.from(user, totalJobs, stats)
     }
 }
