48f1be412219d81838bf777133089bed2ff9d82d
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-05-15 15:51:27 -0300

    Agregado de constantes para referenciar imágenes desde la url del volumen que las contiene

--- src/main/kotlin/quickfix/utils/Constants.kt
+++ src/main/kotlin/quickfix/utils/Constants.kt
@@ -1,9 +1,15 @@
 package quickfix.utils
 
-const val BACKEND_URL = "http://localhost:8080"
 const val FRONTEND_URL = "http://localhost:5173"
-const val AVATAR_URL_BASE = "$BACKEND_URL/images/avatar-" // + userId
-const val CERTIFICATE_URL_BASE = "$BACKEND_URL/images/certificate-" // + certificateId
+
+const val IMAGES_VIEWER_SERVER_URL = "http://localhost:9090"
+const val AVATAR_FILE_NAME = "avatar-"
+const val CERTIFICATE_FILE_NAME = "certificate-"
+const val AVATAR_URL_BASE = "$IMAGES_VIEWER_SERVER_URL/$AVATAR_FILE_NAME" // + userId
+const val CERTIFICATE_URL_BASE = "$IMAGES_VIEWER_SERVER_URL/$CERTIFICATE_FILE_NAME" // + certificateId
+val VALID_IMG_EXTENSIONS = listOf("jpg", "jpeg", "png")
+
 const val MAXIMUM_DEBT: Double = 1000.0
-const val PAGE_SIZE: Int = 4
 const val COMISSION = 0.04
+
+const val PAGE_SIZE: Int = 4
