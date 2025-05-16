f6161eb3115cd4ef87b2f4d0263e158a2c4fcaba
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-05-16 11:08:16 -0300

    Limpieza en Constants.kt

--- src/main/kotlin/quickfix/utils/Constants.kt
+++ src/main/kotlin/quickfix/utils/Constants.kt
@@ -5,10 +5,8 @@ const val FRONTEND_URL = "http://localhost:5173"
 const val IMAGES_UPLOADER_SERVER_URL = "http://localhost:5000/upload"
 const val IMAGES_VIEWER_SERVER_URL = "http://localhost:9090"
 
-const val AVATAR_FILE_NAME = "avatar-"
-const val CERTIFICATE_FILE_NAME = "certificate-"
-const val AVATAR_URL_BASE = "$IMAGES_VIEWER_SERVER_URL/$AVATAR_FILE_NAME" // + userId
-const val CERTIFICATE_URL_BASE = "$IMAGES_VIEWER_SERVER_URL/$CERTIFICATE_FILE_NAME" // + certificateId
+const val AVATAR_FILE_NAME = "avatar-" // + userId
+const val CERTIFICATE_FILE_NAME = "certificate-" // + certificateId
 
 const val MAX_FILE_SIZE = 10 * 1024 * 1024 //10 MB
 val VALID_IMG_EXTENSIONS = listOf("jpg", "jpeg", "png")
