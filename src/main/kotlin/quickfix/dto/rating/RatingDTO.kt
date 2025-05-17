e4efeb29ab7de008cc14da8e66c178b553d31eab
kirinico <kirinico@gmail.com>
2025-05-17 09:42:55 +0900

    se agrego decoradores para q sirva este dto tanto en post como en get y se agrego el campo yearAndMonth

--- src/main/kotlin/quickfix/dto/rating/RatingDTO.kt
+++ src/main/kotlin/quickfix/dto/rating/RatingDTO.kt
@@ -1,16 +1,28 @@
 package quickfix.dto.rating
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import quickfix.models.Rating
+import quickfix.utils.functions.toYearMonthString
 
 data class RatingDTO(
+
+    @field:JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     val jobId: Long,
     val score: Int,
-    val comment: String
+    val comment: String,
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    val yearAndMonth: String? = null
 )
 
-fun Rating.toDTO(): RatingDTO =
-    RatingDTO(
+fun Rating.toDTO(): RatingDTO {
+
+    return RatingDTO(
         jobId = this.job.id,
         score = this.score,
-        comment = this.comment
+        comment = this.comment,
+        yearAndMonth = yearAndMonth.toYearMonthString(),
     )
+
+
+}
