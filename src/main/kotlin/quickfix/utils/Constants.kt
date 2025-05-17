394b9c675d1f85d630b8f40e3a8d5989f32ac0c6
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-05-17 15:10:13 -0300

    Agregado de constante MAX_CUSTOMER_REQUESTS_AT_TIME para especificar el máximo de solicitudes activas al mismo tiempo para un customer.

--- src/main/kotlin/quickfix/utils/Constants.kt
+++ src/main/kotlin/quickfix/utils/Constants.kt
@@ -11,8 +11,9 @@ const val CERTIFICATE_FILE_NAME = "certificate-" // + certificateId
 const val MAX_FILE_SIZE = 10 * 1024 * 1024 //10 MB
 val VALID_IMG_EXTENSIONS = listOf("jpg", "jpeg", "png")
 
-
 const val MAXIMUM_DEBT: Double = 1000.0
 const val COMISSION = 0.04
 
 const val PAGE_SIZE: Int = 4
+
+const val MAX_CUSTOMER_REQUESTS_AT_TIME = 10
