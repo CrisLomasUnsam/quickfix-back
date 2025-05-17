29969dbd7831201030c1be943df35423ffc0dcb3
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-05-17 15:05:11 -0300

    Agregado de propiedad neededDatetime + validación + JsonFormat decorator a JobRequestDTO

--- src/main/kotlin/quickfix/dto/job/jobRequest/JobRequestDTO.kt
+++ src/main/kotlin/quickfix/dto/job/jobRequest/JobRequestDTO.kt
@@ -1,11 +1,14 @@
 package quickfix.dto.job.jobRequest
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 import quickfix.utils.exceptions.CustomerException
 import quickfix.utils.exceptions.DetailException
+import quickfix.utils.exceptions.IllegalDataException
 import quickfix.utils.exceptions.ProfessionException
+import java.time.LocalDateTime
 
 @Schema(description = "Solicitud de un Job por un customer")
 data class JobRequestDTO @JsonCreator constructor(
@@ -28,6 +31,10 @@ data class JobRequestDTO @JsonCreator constructor(
     @JsonProperty("professionName")
     var professionName: String,
 
+    @JsonProperty("neededDatetime")
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    var neededDatetime: LocalDateTime,
+
     @JsonProperty("detail")
     var detail: String,
 
@@ -40,8 +47,23 @@ fun JobRequestDTO.validate() {
     validCustomer(customerId)
     validProfession(professionId)
     validDetail(detail)
+    validDatetime(neededDatetime)
 }
 
-private fun validCustomer(customerId: Long) { if(customerId < 1) throw CustomerException("No existe un customer con ese ID") }
-private fun validProfession(professionId: Long) { if(professionId < 1) throw ProfessionException("El ID de la profesión no matchea con una profesión en la BBDD") }
-private fun validDetail(detail: String) { if(detail.isBlank()) throw DetailException("EL detalle no puede estar vacío") }
+private fun validCustomer(customerId: Long) {
+    if(customerId < 1) throw CustomerException("No existe un customer con ese ID")
+}
+
+private fun validProfession(professionId: Long) {
+    if(professionId < 1) throw ProfessionException("El ID de la profesión no matchea con una profesión en la BBDD")
+}
+
+private fun validDetail(detail: String) {
+    if(detail.isBlank()) throw DetailException("EL detalle no puede estar vacío")
+}
+
+private fun validDatetime(dateTime: LocalDateTime) {
+    if(dateTime.isBefore(LocalDateTime.now().minusMinutes(1))) {
+        throw IllegalDataException("La fecha y hora de solicitud de servicio no pueden ser anteriores a la fecha y hora actuales")
+    }
+}
