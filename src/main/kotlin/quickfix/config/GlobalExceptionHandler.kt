f760c537b9a142639a4cdb34a2f78eeb4a96fb7e
Valentino-afk <valentino.bortolussi.lembo@gmail.com>
2025-05-18 18:07:54 -0300

    Fix Exception Handler.

--- src/main/kotlin/quickfix/config/GlobalExceptionHandler.kt
+++ src/main/kotlin/quickfix/config/GlobalExceptionHandler.kt
@@ -10,48 +10,48 @@ import quickfix.utils.exceptions.*
 class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalDataException::class)
-    fun handleIllegalData(e: IllegalDataException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.message)
+    fun handleIllegalData(e: IllegalDataException): ResponseEntity<SimpleResponse> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(SimpleResponse(e.message))
     }
 
     @ExceptionHandler(NotFoundException::class)
-    fun handleNotFound(e: NotFoundException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.message)
+    fun handleNotFound(e: NotFoundException): ResponseEntity<SimpleResponse> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(SimpleResponse(e.message))
     }
 
     @ExceptionHandler(InvalidCredentialsException::class)
-    fun handleInvalidCredentials(e: InvalidCredentialsException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.message)
+    fun handleInvalidCredentials(e: InvalidCredentialsException): ResponseEntity<SimpleResponse> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(SimpleResponse(e.message))
     }
 
     @ExceptionHandler(RatingException::class)
-    fun handleRating(e: RatingException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.message)
+    fun handleRating(e: RatingException): ResponseEntity<SimpleResponse> {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(SimpleResponse(e.message))
     }
 
     @ExceptionHandler(InvalidTokenException::class)
-    fun handleInvalidToken(e: InvalidTokenException): ResponseEntity<String> {
-        return ResponseEntity.status(498).body(e.message)
+    fun handleInvalidToken(e: InvalidTokenException): ResponseEntity<SimpleResponse> {
+        return ResponseEntity.status(498).body(SimpleResponse(e.message))
     }
 
     @ExceptionHandler(JobException::class)
-    fun handleJob(e: JobException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.message)
+    fun handleJob(e: JobException): ResponseEntity<SimpleResponse> {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(SimpleResponse(e.message))
     }
 
     @ExceptionHandler(AddressException::class)
-    fun handleAddress(e: AddressException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.message)
+    fun handleAddress(e: AddressException): ResponseEntity<SimpleResponse> {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(SimpleResponse(e.message))
     }
 
     @ExceptionHandler(ProfessionalException::class)
-    fun handleProfessional(e: ProfessionalException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.message)
+    fun handleProfessional(e: ProfessionalException): ResponseEntity<SimpleResponse> {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(SimpleResponse(e.message))
     }
 
     @ExceptionHandler(ImageException::class)
-    fun handleImage(e: ImageException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.message)
+    fun handleImage(e: ImageException): ResponseEntity<SimpleResponse> {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(SimpleResponse(e.message))
     }
 
 }
