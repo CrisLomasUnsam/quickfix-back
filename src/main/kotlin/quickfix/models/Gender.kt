cfa1681412a12e18d5215f8cc5e435367200e9de
Santiago Bouza <sbouza@estudiantes.unsam.edu.ar>
2025-05-19 12:29:13 -0300

    refactor compatibilidad con front

--- src/main/kotlin/quickfix/models/Gender.kt
+++ src/main/kotlin/quickfix/models/Gender.kt
@@ -2,14 +2,17 @@ package quickfix.models
 
 import quickfix.utils.exceptions.IllegalDataException
 
-enum class Gender(val genderName: String) {
-    MALE(genderName = "MALE"),
-    FEMALE(genderName = "FEMALE"),
-    OTHER(genderName = "OTHER");
+enum class Gender(val genderName: String, val altValue: String) {
+    MALE(genderName = "MALE", altValue = "Masculino"),
+    FEMALE(genderName = "FEMALE", altValue = "Feminino"),
+    OTHER(genderName = "OTHER", altValue = "Otro");
 
     companion object {
         fun fromName(nombre: String): Gender =
-            entries.find { it.genderName.equals(nombre, ignoreCase = true) }
+            entries.find {
+                it.genderName.equals(nombre, ignoreCase = true) ||
+                it.altValue.equals(nombre, ignoreCase = true)
+            }
                 ?: throw IllegalDataException("Error en parseo de género")
     }
 }
