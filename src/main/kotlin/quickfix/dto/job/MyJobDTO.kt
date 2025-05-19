425f7ae59d55ea069da61df4b68414b18ad1acce
kirinico <kirinico@gmail.com>
2025-05-19 10:48:45 +0900

    se agrego el avatar

--- src/main/kotlin/quickfix/dto/job/MyJobDTO.kt
+++ src/main/kotlin/quickfix/dto/job/MyJobDTO.kt
@@ -1,21 +1,26 @@
 package quickfix.dto.job
 
+import quickfix.utils.functions.getAvatarUrl
 import java.time.LocalDate
 
 interface MyJobProjection {
     fun getId(): Long
     fun getDate(): LocalDate
+    fun getUserId(): Long?
     fun getUserName(): String
     fun getUserLastName(): String
     fun getProfession(): String
     fun getStatus(): String
     fun getPrice(): Double
     fun getScore(): Int?
+
+
 }
 
 fun MyJobProjection.toDto(): MyJobDTO = MyJobDTO(
     id = getId(),
     date = getDate(),
+    avatar = getAvatarUrl(getUserId() ?: 0L),
     userName = getUserName(),
     userLastName = getUserLastName(),
     professionName = getProfession(),
@@ -26,6 +31,7 @@ fun MyJobProjection.toDto(): MyJobDTO = MyJobDTO(
 data class MyJobDTO(
     val id: Long,
     val date: LocalDate,
+    val avatar: String,
     val userName: String,
     val userLastName: String,
     val professionName: String,
