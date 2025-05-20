031b513394c872cfe44c6cb565541844c6fd8a02
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-05-20 14:12:34 -0300

    Agregado de métodos en ratingService para actualizar el averageRating en las tablas de users (customers) y professionals

--- src/main/kotlin/quickfix/services/RatingService.kt
+++ src/main/kotlin/quickfix/services/RatingService.kt
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import quickfix.dao.RatingRepository
 import quickfix.dto.rating.RatingDTO
+import quickfix.dto.rating.RatingInfoDTO
+import quickfix.models.Job
 import quickfix.models.Rating
 import quickfix.utils.exceptions.RatingException
 import java.time.LocalDate
@@ -13,7 +15,6 @@ import java.time.LocalDate
 @Service
 class RatingService(
     val ratingRepository: RatingRepository,
-    val userService: UserService,
     val jobService: JobService
 ) {
 
@@ -30,18 +31,20 @@ class RatingService(
         ratingRepository.findByJobId(jobId).orElseThrow { RatingException("Ha habido un error al obtener la calificación.") }
 
     @Transactional(rollbackFor = [Exception::class])
-    fun rateUser(currentUserId: Long, ratingDTO: RatingDTO) {
+    fun rateUser(raterUserId: Long, ratingDTO: RatingDTO) {
 
-        val userFrom = userService.getById(currentUserId)
-        val job = jobService.getJobById(ratingDTO.jobId)
-
-        if (currentUserId != job.customer.id && currentUserId != job.professional.id)
-            throw RatingException("Usted no puede calificar este servicio.")
-
-        if (ratingRepository.existsByJobIdAndUserFromId(job.id, userFrom.id))
+        if (ratingRepository.existsByJobIdAndUserFromId(ratingDTO.jobId, raterUserId))
             throw RatingException("No puede agregar más calificaciones para este servicio.")
 
-        val userTo = if (currentUserId == job.customer.id) job.professional else job.customer
+        val job : Job = jobService.getJobById(ratingDTO.jobId)
+        val raterIsCustomer = raterUserId == job.customer.id
+        val userFrom = if (raterIsCustomer) job.customer else job.professional
+        val userTo = if (raterIsCustomer) job.professional else job.customer
+
+        if(raterIsCustomer)
+            addScoreRatingToProfessional(userTo.id, ratingDTO.score)
+        else
+            addScoreRatingToCustomer(userTo.id, ratingDTO.score)
 
         val rating = Rating().apply {
             this.userFrom = userFrom
@@ -56,16 +59,53 @@ class RatingService(
     }
 
     @Transactional(rollbackFor = [Exception::class])
+    fun addScoreRatingToCustomer(customerId: Long, score: Int) {
+        val ratingInfo = RatingInfoDTO.toDTO(ratingRepository.findCustomerRatingInfo(customerId))
+        val newAverageRating = (ratingInfo.total + score) / (ratingInfo.amount + 1.0)
+        ratingRepository.setAverageRatingForCustomer(customerId, newAverageRating)
+    }
+
+    @Transactional(rollbackFor = [Exception::class])
+    fun addScoreRatingToProfessional(professionalId: Long, score: Int) {
+        val ratingInfo = RatingInfoDTO.toDTO(ratingRepository.findProfessionalRatingInfo(professionalId))
+        val newAverageRating = (ratingInfo.total + score) / (ratingInfo.amount + 1.0)
+        ratingRepository.setAverageRatingForProfessional(professionalId, newAverageRating)
+    }
+
+    @Transactional(rollbackFor = [Exception::class])
     fun updateRating(userId: Long, data: RatingDTO) {
 
         if(!ratingRepository.existsByJobIdAndUserFromId(data.jobId, userId))
             throw RatingException("Ha habido un error al modificar esta calificación.")
 
-        val rating = getByJobId(data.jobId)
+        val rating : Rating = getByJobId(data.jobId)
+        val raterIsCustomer = userId == rating.job.customer.id
+
+        if(raterIsCustomer)
+            changeProfessionalScoreRating(rating, data.score)
+        else
+            changeCustomerScoreRating(rating, data.score)
+
         rating.apply {
             score = data.score
             comment = data.comment
         }.also { it.validate() }
+    }
+
+    @Transactional(rollbackFor = [Exception::class])
+    fun changeCustomerScoreRating(rating: Rating, newScore: Int) {
+        val customerId = rating.userTo.id
+        val ratingInfo = RatingInfoDTO.toDTO(ratingRepository.findCustomerRatingInfo(customerId))
+        val newAverageRating = (ratingInfo.total + newScore - rating.score) / (ratingInfo.amount)
+        ratingRepository.setAverageRatingForCustomer(customerId, newAverageRating)
+    }
+
+    @Transactional(rollbackFor = [Exception::class])
+    fun changeProfessionalScoreRating(rating: Rating, newScore: Int) {
+        val professionalId = rating.userTo.id
+        val ratingInfo = RatingInfoDTO.toDTO(ratingRepository.findProfessionalRatingInfo(professionalId))
+        val newAverageRating = (ratingInfo.total + newScore - rating.score) / (ratingInfo.amount)
+        ratingRepository.setAverageRatingForProfessional(professionalId, newAverageRating)
     }
 
 }
