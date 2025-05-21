ca19ac69e2aa003f298657276ff2e44e98bff13a
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-05-21 09:56:56 -0300

    Agregado de parámetros faltantes en ProfessionalJobOfferDTO

--- src/main/kotlin/quickfix/dto/job/jobOffer/ProfessionalJobOfferDTO.kt
+++ src/main/kotlin/quickfix/dto/job/jobOffer/ProfessionalJobOfferDTO.kt
@@ -1,30 +1,38 @@
 package quickfix.dto.job.jobOffer
 
-import quickfix.dto.user.SeeUserBasicInfoDTO
+import quickfix.dto.user.SeeBasicUserInfoDTO
+import quickfix.utils.functions.DatetimeFormatter
 
 data class ProfessionalJobOfferDTO (
-    var professionalId : Long,
+    var requestId: String,
     var professionId : Long,
-    var customer: SeeUserBasicInfoDTO,
+    var customer: SeeBasicUserInfoDTO,
     var price: Double,
     var distance: Double,
+    var detail: String,
     var estimatedArriveTime: Int,
     var jobDuration: Int,
     var jobDurationTimeUnit: String,
+    var neededDatetime: String,
+    var instantRequest: Boolean
 
     )
 {
     companion object {
         fun fromDto(jobOfferDTO: JobOfferDTO): ProfessionalJobOfferDTO {
+            val requestId = "${jobOfferDTO.profession.id}_${jobOfferDTO.customer.id}"
             return ProfessionalJobOfferDTO(
-                professionalId = jobOfferDTO.professional.id,
+                requestId = requestId,
                 professionId = jobOfferDTO.profession.id,
                 customer = jobOfferDTO.customer,
                 price = jobOfferDTO.price,
                 distance = jobOfferDTO.distance,
+                detail = jobOfferDTO.detail,
                 estimatedArriveTime = jobOfferDTO.estimatedArriveTime,
                 jobDuration = jobOfferDTO.jobDuration,
-                jobDurationTimeUnit = jobOfferDTO.jobDurationTimeUnit
+                jobDurationTimeUnit = jobOfferDTO.jobDurationTimeUnit,
+                neededDatetime = jobOfferDTO.neededDatetime.format(DatetimeFormatter),
+                instantRequest = jobOfferDTO.instantRequest
             )
         }
     }
