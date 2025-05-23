cea2398a8cffd05eb8e69d52216fac396b231944
kirinico <kirinico@gmail.com>
2025-05-23 10:36:24 +0900

    modifique el date para mandarle dd/MM/yyyy HH:mm

--- src/main/kotlin/quickfix/dto/job/jobRequest/CreateJobRequestDTO.kt
+++ src/main/kotlin/quickfix/dto/job/jobRequest/CreateJobRequestDTO.kt
@@ -5,20 +5,28 @@ import quickfix.utils.MINUTES_TO_BE_CONSIDERED_FUTURE_REQUEST
 import quickfix.utils.exceptions.DetailException
 import quickfix.utils.exceptions.IllegalDataException
 import quickfix.utils.exceptions.ProfessionException
+import quickfix.utils.functions.DatetimeFormatter
+import quickfix.utils.functions.parseDatetime
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 data class CreateJobRequestDTO(
     val userId: Long,
     var serviceId: Long,
     var detail: String,
-    val neededDatetime: LocalDateTime,
+    val neededDatetime: String,
     var instantRequest: Boolean = false
 ){
+
+
 
     fun validate() {
         if (serviceId < 1) throw ProfessionException("ID de profesión inválido")
         if (detail.isBlank()) throw DetailException("El detalle no puede estar vacío")
-        if (neededDatetime.isBefore(LocalDateTime.now().minusMinutes(MINUTES_TO_BE_CONSIDERED_FUTURE_REQUEST))) {
+
+        val parsedDatetime = parseDatetime(neededDatetime)
+        if (parsedDatetime.isBefore(LocalDateTime.now().minusMinutes(MINUTES_TO_BE_CONSIDERED_FUTURE_REQUEST))) {
             throw IllegalDataException("La fecha y hora no pueden ser anteriores a la fecha actual")
         }
     }
@@ -28,6 +36,6 @@ data class CreateJobRequestDTO(
             customer = customer,
             professionId = this.serviceId,
             detail = this.detail,
-            neededDatetime = this.neededDatetime
+            neededDatetime = parseDatetime(neededDatetime)
         )
 }
