22ded8f3652cd18d2356ac2aada03129945dc51e
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-05-23 17:15:03 -0300

    Agregado de método PageDTO.ToRatingsDTO

--- src/main/kotlin/quickfix/dto/page/PageDTO.kt
+++ src/main/kotlin/quickfix/dto/page/PageDTO.kt
@@ -2,9 +2,12 @@ package quickfix.dto.page
 
 import org.springframework.data.domain.Page
 import quickfix.dto.job.JobWithRatingDTO
+import quickfix.dto.rating.RatingDTO
+import quickfix.dto.rating.toDTO
+import quickfix.models.Rating
 
 data class PageDTO<T>(
-    var content: MutableList<T>,
+    var content: List<T>,
     var first: Boolean,
     var last: Boolean,
     var empty: Boolean,
@@ -15,6 +18,17 @@ data class PageDTO<T>(
         fun toJobWithRatingPageDTO(page: Page<JobWithRatingDTO>): PageDTO<JobWithRatingDTO> {
             return PageDTO(
                 content = page.content,
+                first = page.isFirst,
+                last = page.isLast,
+                empty = page.isEmpty,
+                currentPage = page.number,
+                totalPages = page.totalPages
+            )
+        }
+
+        fun toRatingsDTO(page: Page<Rating>): PageDTO<RatingDTO> {
+            return PageDTO(
+                content = page.content.map {it.toDTO()},
                 first = page.isFirst,
                 last = page.isLast,
                 empty = page.isEmpty,
