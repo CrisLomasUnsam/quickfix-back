c44455a06509a2664d752265e778f52193aedc84
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-06-04 01:53:24 -0300

    Ajuste en SecurityConfig para agregar nuevos endpoints relacionados a direcciones secundarias

--- src/main/kotlin/quickfix/config/SecurityConfig.kt
+++ src/main/kotlin/quickfix/config/SecurityConfig.kt
@@ -112,6 +112,11 @@ class SecurityConfig {
                 it.requestMatchers(HttpMethod.GET, "/user/seeProfessionalProfile/**").hasAuthority(Role.CUSTOMER.name)
                 it.requestMatchers(HttpMethod.GET, "/user/seeBasicProfessionalInfo/**").hasAuthority(Role.CUSTOMER.name)
                 it.requestMatchers(HttpMethod.GET, "/user/**").hasAnyAuthority(Role.CUSTOMER.name, Role.PROFESSIONAL.name)
+
+                it.requestMatchers(HttpMethod.GET, "/user/secondaryAddress").hasAuthority(Role.CUSTOMER.name)
+                it.requestMatchers(HttpMethod.POST, "/user/secondaryAddress").hasAuthority(Role.CUSTOMER.name)
+                it.requestMatchers(HttpMethod.DELETE, "/user/secondaryAddress").hasAuthority(Role.CUSTOMER.name)
+
                 it.requestMatchers(HttpMethod.PATCH, "/user/**").hasAnyAuthority(Role.CUSTOMER.name, Role.PROFESSIONAL.name)
 
                 it.anyRequest().authenticated()
