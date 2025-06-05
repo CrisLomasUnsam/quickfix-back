a2799e881a5fdd089aab54fb72d2ffcd8267ff2c
Cris Lomas <clomas@estudiantes.unsam.edu.ar>
2025-06-04 21:32:11 -0300

    Agregado de validación para jobRequests para que solo puedan realizarse solicitudes dentro de los próximos 28 días

--- src/main/kotlin/quickfix/dto/job/jobRequest/CreateJobRequestDTO.kt
+++ src/main/kotlin/quickfix/dto/job/jobRequest/CreateJobRequestDTO.kt
@@ -1,6 +1,7 @@
 package quickfix.dto.job.jobRequest
 
 import quickfix.dto.user.SeeBasicUserInfoDTO
+import quickfix.utils.INSTANT_REQUEST_LIVE_DAYS
 import quickfix.utils.MINUTES_TO_BE_CONSIDERED_FUTURE_REQUEST
 import quickfix.utils.exceptions.DetailException
 import quickfix.utils.exceptions.IllegalDataException
@@ -31,6 +32,10 @@ data class CreateJobRequestDTO(
         //Si la fecha y hora son anteriores a 5 minutos en el PASADO, lanzamos error
         if (parsedDatetime.isBefore(LocalDateTime.now().minusMinutes(MINUTES_TO_BE_CONSIDERED_FUTURE_REQUEST)))
             throw IllegalDataException("La fecha y hora no pueden ser anteriores a la fecha actual")
+
+        //Si la fecha y hora son posteriores a los días que definimos como tiempo de vida de las requests, lanzamos error
+        if (parsedDatetime.isAfter(LocalDateTime.now().plusDays(INSTANT_REQUEST_LIVE_DAYS)))
+            throw IllegalDataException("Debe elegir una fecha que esté dentro de los próximos $INSTANT_REQUEST_LIVE_DAYS días.")
 
         //Si la fecha y hora son anteriores a 5 minutos en el FUTURO, consideramos que es una instantRequest
         if (parsedDatetime.isBefore(LocalDateTime.now().plusMinutes(MINUTES_TO_BE_CONSIDERED_FUTURE_REQUEST)))
