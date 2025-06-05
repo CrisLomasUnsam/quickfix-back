0a0dc1e074a67f7227b052e6b8af2ef910a8f656
KZvilla <krisvilla93@gmail.com>
2025-06-04 21:18:49 -0300

    refactor: se agrega endpoint para obtener la suscripción del profesional

--- src/main/kotlin/quickfix/controllers/ProfessionalController.kt
+++ src/main/kotlin/quickfix/controllers/ProfessionalController.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import quickfix.dto.mercadopago.SubscriptionStatusDTO
 import quickfix.dto.professional.FinancesDTO
 import quickfix.dto.professional.NewCertificateDTO
 import quickfix.dto.professional.ProfessionDTO
@@ -23,6 +24,68 @@ class ProfessionalController(
         val usernamePAT = SecurityContextHolder.getContext().authentication
         return usernamePAT.principal.toString().toLong()
     }
+
+    /**
+     * Obtiene la suscripción del profesional. Las posibles combinaciones de respuesta son:
+     *
+     * 1. El profesional **nunca tuvo suscripción** y **no está en período de prueba**:
+     *    ```json
+     *    {
+     *      "subscriptionId": null,
+     *      "status": "NONE",
+     *      "nextPaymentDate": null
+     *    }
+     *    ```
+     *
+     * 2. El profesional **no tiene suscripción activa** pero **está en período de prueba**:
+     *    ```json
+     *    {
+     *      "subscriptionId": null,
+     *      "status": "AUTHORIZED",
+     *      "nextPaymentDate": "20xx-12-01T00:00:00Z" // fecha de finalización del trial (30 días desde su activación)
+     *    }
+     *    ```
+     *
+     * 3. El profesional **tuvo una suscripción activa**, pero **ya venció o la cancelo(no confundir con cancelled)** y **ya tuvo periodo de prueba**:
+     *    ```json
+     *    {
+     *      "subscriptionId": "1234567890bacdef",
+     *      "status": "PAUSED",
+     *      "nextPaymentDate": "20xx-12-01T00:00:00Z" // fecha que estaria en el pasado.
+     *    }
+     *    ```
+     * 4. El profesional **tiene una suscripción activa**:
+     *    ```json
+     *    {
+     *      "id": "1234567890bacdef",
+     *      "status": "AUTHORIZED"
+     *      "nextPaymentDate": "20xx-12-01T00:00:00Z" // fecha valida.
+     *    }
+     *    ```
+     * 5. El profesional **tiene una suscripcion activa** pero **pendiente** de pago:
+     *    ```json
+     *    {
+     *      "id": "1234567890bacdef",
+     *      "status": "PENDING",
+     *      "nextPaymentDate": "20xx-12-01T00:00:00Z" // fecha valida.
+     *    }
+     *    ```
+     * 6. El profesional **tuvo una suscripcion activa** pero **por motivos externos** se canceló la misma, el id NO ES VALIDO DEBE BORRARSE dado que no se puede reactivar, :
+     *    ```json
+     *    {
+     *      "id": "1234567890bacdef",
+     *      "status": "CANCELLED",
+     *      "nextPaymentDate": "20xx-12-01T00:00:00Z" // fecha valida.
+     *    }
+     *    ```
+     */
+
+    @GetMapping("/subscription")
+    @Operation(summary = "Obtener la suscripción del profesional")
+    fun getProfessionalSubscription(@ModelAttribute("currentProfessionalId") currentProfessionalId: Long): SubscriptionStatusDTO =
+        professionalService.getProfessionalSubscription(currentProfessionalId)
+            .let { SubscriptionStatusDTO.toDTO(it) }
+
 
     @GetMapping("/finances")
     @Operation(summary = "Finanzas del profesional (debt y balance)")
@@ -44,8 +107,9 @@ class ProfessionalController(
 
     @GetMapping("/professions")
     @Operation(summary = "Obtener los servicios que puede brindar el profesional")
-    fun getProfessionalProfessions(@ModelAttribute("currentProfessionalId") currentProfessionalId: Long) : List<ProfessionDTO> =
-        professionalService.getProfessionalProfessions(currentProfessionalId).map { ProfessionDTO.toDto(it) }.sortedBy { it.name }
+    fun getProfessionalProfessions(@ModelAttribute("currentProfessionalId") currentProfessionalId: Long): List<ProfessionDTO> =
+        professionalService.getProfessionalProfessions(currentProfessionalId).map { ProfessionDTO.toDto(it) }
+            .sortedBy { it.name }
 
     @PostMapping("/profession")
     @Operation(summary = "Agregar servicio brindado")
@@ -65,12 +129,18 @@ class ProfessionalController(
 
     @PatchMapping("/profession/activate")
     @Operation(summary = "Activar servicio brindado")
-    fun activateProfession(@ModelAttribute("currentProfessionalId") currentProfessionalId: Long, @RequestParam professionId: Long) =
+    fun activateProfession(
+        @ModelAttribute("currentProfessionalId") currentProfessionalId: Long,
+        @RequestParam professionId: Long
+    ) =
         professionalService.switchProfessionStatus(currentProfessionalId, professionId, activate = true)
 
     @PatchMapping("/profession/deactivate")
     @Operation(summary = "Desactivar servicio brindado")
-    fun deactivateProfession(@ModelAttribute("currentProfessionalId") currentProfessionalId: Long, @RequestParam professionId: Long) =
+    fun deactivateProfession(
+        @ModelAttribute("currentProfessionalId") currentProfessionalId: Long,
+        @RequestParam professionId: Long
+    ) =
         professionalService.switchProfessionStatus(currentProfessionalId, professionId, activate = false)
 
     @GetMapping("/certificates")
@@ -80,16 +150,26 @@ class ProfessionalController(
 
     @PostMapping("/certificate")
     @Operation(summary = "Agregar certificado")
-    fun addCertificate(@ModelAttribute("currentProfessionalId") currentProfessionalId: Long, @RequestBody certificateDto: NewCertificateDTO) =
+    fun addCertificate(
+        @ModelAttribute("currentProfessionalId") currentProfessionalId: Long,
+        @RequestBody certificateDto: NewCertificateDTO
+    ) =
         professionalService.addCertificate(currentProfessionalId, certificateDto)
 
     @PostMapping("/certificate/{certificateId}")
     @Operation(summary = "Agregar certificado")
-    fun uploadCertificateImg(@ModelAttribute("currentProfessionalId") currentProfessionalId: Long, @PathVariable certificateId : Long, @RequestBody image: MultipartFile) =
+    fun uploadCertificateImg(
+        @ModelAttribute("currentProfessionalId") currentProfessionalId: Long,
+        @PathVariable certificateId: Long,
+        @RequestBody image: MultipartFile
+    ) =
         professionalService.uploadCertificateImg(currentProfessionalId, certificateId, image)
 
     @DeleteMapping("/certificate/{certificateId}")
     @Operation(summary = "Borrar un certificado")
-    fun deleteCertificate(@ModelAttribute("currentProfessionalId") currentProfessionalId: Long, @PathVariable certificateId : Long) =
+    fun deleteCertificate(
+        @ModelAttribute("currentProfessionalId") currentProfessionalId: Long,
+        @PathVariable certificateId: Long
+    ) =
         professionalService.deleteCertificate(currentProfessionalId, certificateId)
 }
